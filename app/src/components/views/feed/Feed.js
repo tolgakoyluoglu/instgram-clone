008e150ee8a5c6d189d3e596f6048ce0dddf8c96
tolgakoyluoglu <tolga.koyluoglu@chasacademy.se>
2019-11-23 17:47:10 +0100

    fix: remove models, restructure files in frontend

---
+++ app/src/components/views/feed/Feed.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import Posts from './Posts';
+import { CREATE_POST, GET_POSTS } from '../../../shared/utils/graphql';
+import { LoadingContainer, Loader } from '../../../styled/Loading';
+
+const Feed = () => {
+  const [image, setImage] = React.useState('');
+  const [title, setTitle] = React.useState('');
+
+  const [createPost, { error, loading }] = useMutation(CREATE_POST, {
+    variables: { title: title, url: image },
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: GET_POSTS
+      });
+      data.posts.push(result.data.createPost);
+      proxy.writeQuery({ query: GET_POSTS, data: { ...data } });
+    }
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    createPost();
+    setTitle('');
+    setImage('');
+  };
+
+  if (error) {
+    console.log(error.message);
+  }
+
+  if (loading)
+    return (
+      <LoadingContainer>
+        <Loader />
+      </LoadingContainer>
+    );
+
+  return (
+    <div>
+      <h1>Feed</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={title}
+          type="text"
+          placeholder="Title"
+          required
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          value={image}
+          type="text"
+          placeholder="Image"
+          required
+          onChange={e => setImage(e.target.value)}
+        />
+        <button type="submit">Add Photo</button>
+      </form>
+      <Posts />
+    </div>
+  );
+};
+
+export default Feed;
